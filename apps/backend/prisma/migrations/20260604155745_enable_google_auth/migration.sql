adbaea68cf7d273662ca089e1bcdb3395674bf6e
Kayden_0811 <121278115+KaydenAu@users.noreply.github.com>
2026-06-06 22:31:35 +0800

    Refactor: Updates on Database Fields

---
+++ apps/backend/prisma/migrations/20260604155745_enable_google_auth/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `custom_users` MODIFY `password_hash` VARCHAR(191) NULL;
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,16 +1,42 @@
-import express from "express";
+import cors from "cors";
+import dotenv from 'dotenv';
+import express, { Request, Response, NextFunction } from "express";
+
+import authRouter from "./modules/auth/auth.api"
+import passport from "./modules/auth/integrations_google/google.passport"
+
+dotenv.config();
+const port = process.env.PORT;
+const frontendUrl = process.env.FRONTEND_URL;
 
 const app = express();
+app.use(express.static('public'));
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-    res.send('Hello Express!');
+// Permits your Angular frontend application to safely stream network requests
+const corsOptions = {
+  origin: frontendUrl,
+  method: 'GET, HEAD, POST, PATCH, PUT, DELETE',
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization']
+}
+app.use(cors(corsOptions));
+
+// Mounts the authentication module.
+app.use('/api/v1/auth', authRouter);
+app.use(passport.initialize());
+
+// Intercepts errors passed via next(error) from your async route handlers
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  console.error('Unhandled server exception caught:', error);
+  
+  const statusCode = error.status || 500;
+  res.status(statusCode).json({
+    message: error.message || 'An unexpected internal server error occurred.',
+  });
 });
 
-app.get("/health", (req, res) => {
-  res.json({ status: "ok" });
-});
-
-app.listen(3000, () => {
-  console.log("Backend running on port 3000");
+app.listen(port, () => {
+  console.log(`[SERVER] Backend active and listening on port ${port}`);
 });
