fe37b50109d4a91cb980800a4a8b0b2b99c3da94
Kayden_0811 <121278115+KaydenAu@users.noreply.github.com>
2026-06-06 23:12:30 +0800

    Fix: Adjust the Folder Structure for Auth Module

--- apps/frontend/src/app/features/auth/privacy-policy-page/privacy-policy-page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-privacy-policy-page',
-  imports: [],
-  templateUrl: './privacy-policy-page.html',
-  styleUrl: './privacy-policy-page.scss',
-})
-export class PrivacyPolicyPage {}
--- apps/frontend/src/app/features/auth/terms-of-services-page/terms-of-services-page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-terms-of-services-page',
-  imports: [],
-  templateUrl: './terms-of-services-page.html',
-  styleUrl: './terms-of-services-page.scss',
-})
-export class TermsOfServicesPage {}
