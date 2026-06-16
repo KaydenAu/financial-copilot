20d2183f83d207aa52b0ea55cd4fc1d5bce1485f
Kayden_0811 <121278115+KaydenAu@users.noreply.github.com>
2026-06-17 01:07:02 +0800

    Feature: Introduce Core Layout for Core Pages

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/auth/guards/auth-guard';
+import { CoreLayout } from '../shared/layouts/core-layout/core-layout';
+
 import { LoginPage } from './features/auth/pages/login-page/login-page';
 import { RegisterPage } from './features/auth/pages/register-page/register-page';
 import { ForgotPasswordPage } from './features/auth/pages/forgot-password-page/forgot-password-page';
@@ -9,62 +12,41 @@ import { ContactSupportPage } from './features/auth/pages/contact-support-page/c
 import { Error404Page } from './features/auth/pages/error404-page/error404-page';
 import { DashboardPage } from './features/dashboard/pages/dashboard-page/dashboard-page';
 import { OauthCallbackPage } from './features/auth/pages/oauth-callback-page/oauth-callback-page';
-import { authGuard } from './core/auth/guards/auth-guard';
+import { PersonalInfoPage } from './features/profile/pages/personal-info-page/personal-info-page';
+import { SecurityPage } from './features/profile/pages/security-page/security-page';
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'auth/login',
-        pathMatch: 'full',
-    },
+    { path: '', redirectTo: 'auth/login', pathMatch: 'full'},
     {
         path: 'auth',
         children:[
-            {
-                path: 'login', 
-                component: LoginPage,
-            },
-            {
-                path: 'register', 
-                component: RegisterPage,
-            },
-            {
-                path: 'forgot-password', 
-                component: ForgotPasswordPage,
-            },
-            {
-                path: 'reset-password', 
-                component: ResetPasswordPage,
-            },
-            {
-                path: 'terms-of-services', 
-                component: TermsOfServicesPage,
-            },
-            {
-                path: 'privacy-policy', 
-                component: PrivacyPolicyPage,
-            },
-            {
-                path: 'contact-support',
-                component: ContactSupportPage,
-            },
-            {
-                path: 'errors',
-                component: Error404Page,
-            },
-            {
-                path: 'oauth-callback',
-                component: OauthCallbackPage,
-            },
+            { path: 'login', component: LoginPage},
+            { path: 'register', component: RegisterPage},
+            { path: 'forgot-password', component: ForgotPasswordPage},
+            { path: 'reset-password', component: ResetPasswordPage},
+            { path: 'terms-of-services', component: TermsOfServicesPage},
+            { path: 'privacy-policy', component: PrivacyPolicyPage},
+            { path: 'contact-support', component: ContactSupportPage},
+            { path: 'errors', component: Error404Page},
+            { path: 'oauth-callback', component: OauthCallbackPage},
         ],
     },
     {
-        path: 'dashboard',
-        canActivate:[authGuard],
-        component: DashboardPage,
+        path: '',
+        component: CoreLayout,
+        canActivate: [authGuard],
+        children:[
+            { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+            { path: 'dashboard', component: DashboardPage},
+            {
+                path: 'profile',
+                children: [
+                    { path: '', redirectTo: 'personal-info', pathMatch: 'full' },
+                    { path: 'personal-info', component: PersonalInfoPage },
+                    { path: 'security', component: SecurityPage }
+                ]
+            }
+        ]
     },
-    {
-        path: '**',
-        redirectTo: 'auth/errors'
-    },
+    { path: '**', redirectTo: 'auth/errors'},
 ];
