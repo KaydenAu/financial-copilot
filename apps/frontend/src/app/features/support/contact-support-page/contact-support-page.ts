0ce120114cedac26cd71b56ec73fff97c41ee16b
Nik Afiq <nikafiqdev@gmail.com>
2026-06-23 23:56:09 +0800

    Feat: integrate support backend with Angular frontend

--- apps/frontend/src/app/features/support/contact-support-page/contact-support-page.ts
+++ apps/frontend/src/app/features/support/contact-support-page/contact-support-page.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { SharedModules } from '../../../../shared/shared.module';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { SupportService } from '../support.service';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ChangeDetectorRef } from '@angular/core';
 
 interface Category {
   value: string;
@@ -9,20 +13,74 @@ interface Category {
 
 @Component({
   selector: 'app-contact-support-page',
-  imports: [...SharedModules, MatExpansionModule],
+  imports: [...SharedModules, MatExpansionModule, MatProgressSpinnerModule,
+    MatSnackBarModule],
   templateUrl: './contact-support-page.html',
   styleUrl: './contact-support-page.scss',
 })
+
 export class ContactSupportPage {
-  selectedValue!: string;
+  selectedValue = '';
+  name = '';
+  email = '';
+  message = '';
+  loading = false;
+
+  constructor(
+    private supportService: SupportService,
+    private snackBar: MatSnackBar,
+    private cdr: ChangeDetectorRef
+  ) { }
 
   categories: Category[] = [
-    { value: 'account', viewValue: 'Account Issues' },
-    { value: 'transactions', viewValue: 'Transactions' },
-    { value: 'billing', viewValue: 'Billing & Subscription' },
-    { value: 'security', viewValue: 'Security & Privacy' },
-    { value: 'technical', viewValue: 'Technical Support' },
-    { value: 'feature', viewValue: 'Feature Request' }
+    { value: 'ACCOUNT', viewValue: 'Account Issues' },
+    { value: 'TRANSACTIONS', viewValue: 'Transactions' },
+    { value: 'BILLING', viewValue: 'Billing & Subscription' },
+    { value: 'SECURITY', viewValue: 'Security & Privacy' },
+    { value: 'TECHNICAL', viewValue: 'Technical Support' },
+    { value: 'FEATURE', viewValue: 'Feature Request' }
   ];
 
+  submitForm() {
+    if (this.loading) return;
+    this.loading = true;
+    const payload = {
+      name: this.name,
+      email: this.email,
+      category: this.selectedValue,
+      message: this.message
+    };
+
+    this.supportService.createTicket(payload).subscribe({
+      next: () => {
+        this.loading = false;
+        this.snackBar.open(
+          'Support request sent successfully',
+          'Close',
+          { duration: 3000 }
+        );
+
+        queueMicrotask(() => {
+          this.resetForm();
+          this.cdr.detectChanges();
+        });
+      },
+
+      error: (err) => {
+        this.loading = false;
+        this.snackBar.open(
+          err?.error?.message || 'Something went wrong',
+          'Close',
+          { duration: 3000 }
+        );
+      }
+    });
+  }
+
+  resetForm() {
+    this.name = '';
+    this.email = '';
+    this.selectedValue = '';
+    this.message = '';
+  }
 }
